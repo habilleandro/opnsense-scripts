1dd30e115c15e45e9087ce3d21d6b4526eb964dc
habilleandro <leandro@habil.eti.br>
2025-07-23 13:50:21 -0300

    update

---
+++ scripts/github.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Caminho local do repositório
+REPO_DIR="/usr/local/scripts/opnsense-zbx"
+# URL do repositório remoto
+REPO_URL="https://github.com/habilleandro/opnsense-zbx.git"
+
+# Se o diretório ainda não existe, clona o repositório
+if [ ! -d "$REPO_DIR/.git" ]; then
+    git clone "$REPO_URL" "$REPO_DIR"
+else
+    cd "$REPO_DIR" || exit 1
+    git fetch origin
+
+    # Verifica se há mudanças
+    LOCAL_HASH=$(git rev-parse HEAD)
+    REMOTE_HASH=$(git rev-parse origin/main)
+
+    if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+        echo "Atualizações encontradas. Fazendo pull..."
+        git pull
+    else
+        echo "Sem mudanças no repositório."
+    fi
+fi
