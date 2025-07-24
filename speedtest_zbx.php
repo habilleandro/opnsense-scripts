b8db2dafaa45f4bc18f0bb50fb1a5b052ee9fe7c
habilleandro <leandro@habil.eti.br>
2025-07-24 11:15:47 -0300

    Create speedtest_zbx.php

---
+++ speedtest_zbx.php
@@ -0,0 +1,40 @@
+<?php
+
+$metric = $argv[1] ?? 'download';
+$csvFile = '/usr/local/opnsense/scripts/OPNsense/speedtest/speedtest.csv';
+
+if (!file_exists($csvFile)) {
+    echo "0\n";
+    exit(1);
+}
+
+$lines = file($csvFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$lastLine = end($lines);
+$data = str_getcsv($lastLine);
+
+// Verifica se tem colunas suficientes
+if (count($data) < 8) {
+    echo "0\n";
+    exit(1);
+}
+
+switch ($metric) {
+    case 'download':
+        echo $data[5]; // Download em Mbps
+        break;
+    case 'upload':
+        echo $data[6]; // Upload em Mbps
+        break;
+    case 'ping':
+        echo $data[7]; // Ping em ms
+        break;
+    case 'url':
+        echo $data[8]; // Link do resultado
+        break;
+    case 'provider':
+        echo $data[3]; // Nome do provedor e cidade
+        break;
+    default:
+        echo "0\n";
+        exit(1);
+}
